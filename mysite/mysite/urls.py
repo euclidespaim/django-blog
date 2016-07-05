10ee832e06ef2c9fc2bef9fda61ee33956b2419d
euclidespaim <euclidespaim@gmail.com>
2016-07-05 17:14:29 -0300

    Segurança implementada!!

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 
 from django.conf.urls import include, url
+import django.contrib.auth.views
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', django.contrib.auth.views.login),
     url(r'', include('blog.urls')),
 ]
